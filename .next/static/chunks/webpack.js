add1a1c574106224ea4415cfb2a333aafac27c35
kleitong1 <44074264+kleitong1@users.noreply.github.com>
2022-01-28 14:21:51 -0300

    att1

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3691d5e9e32758cb"; }
+/******/ 		__webpack_require__.h = function() { return "7697c2ab10bf7744"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -168,7 +168,7 @@ export default function PaginaInicial() {
                         />
                         <Button
                             type='submit'
-                            label='Entrar'
+                            label='Entrar 00001'
                             fullWidth
                             buttonColors={{
                                 contrastColor: appConfig.theme.colors.neutrals["000"],
